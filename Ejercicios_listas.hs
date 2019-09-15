e156cacac4b646e8fe0c7e3d144c615978ca0e0b
Forson666 <51457747+Forson666@users.noreply.github.com>
2019-09-14 22:29:50 -0500

    Ejercicios con listas en haskell

--- Ejercicios_listas.hs
+++ Ejercicios_listas.hs
@@ -8,20 +8,39 @@ mayores:: [[Int]]->[Int]
 mayores []=[]
 mayores lista= [mayor(x) | x <- lista]
 
-sumar::[Int]->Int
-sumar [ ] = 0
-sumar (x:xs) = x + sumar(xs)
-
 menor::[Int]->Int
 menor [x] = x
 menor (x:xs) | x < menor(xs) = x | otherwise = menor(xs)
 
-promedio::[Int]->Int
-promedio lista = div (sumar lista) (length lista)
+sumar::(Ord a, Fractional a) =>[a]->a
+sumar [ ] = 0
+sumar (x:xs) = x + sumar(xs)
 
-promedios::[[Int]]->[Int]
+promedio :: (Ord a, Fractional a) => [a] -> a
+promedio [] = 0
+promedio xs = sumar xs / fromIntegral (length xs)
+
+promedios :: (Ord a, Fractional a) => [[a]] -> [a]
 promedios lista = [promedio x | x <- lista]
 
-mascercano:: [Int]->Int->Int
+mascercano:: (Ord a, Fractional a) => [a]->a->a
 mascercano [a] b = a
-mascercano (x:xs) b | abs (x - b) < (mascercano (xs) b) = x | otherwise = (mascercano xs b)
+mascercano (x:xs) b | abs (x - b) < abs (mascercano (xs) b - b) = x | otherwise = mascercano xs b
+
+mascercanos:: (Ord a, Fractional a) => [[a]]->[a]
+mascercanos []=[]
+mascercanos lista= [mascercano x (promedio x) | x <- lista]
+
+divisible::Int->Int->Bool
+divisible x y = (mod x y) ==0
+
+cant_multiplos::[Int]->Int->Int
+cant_multiplos [x] a | (mod x a) == 0  && x /= a = 1 | otherwise = 0
+cant_multiplos (x:xs) a | (mod x a) == 0  && x /= a = 1 + cant_multiplos xs a | otherwise = cant_multiplos xs a
+
+cant_divisores::[Int]->Int->Int
+cant_divisores [x] a | (mod a x) == 0  && x /= a = 1 | otherwise = 0
+cant_divisores (x:xs) a | (mod a x) == 0  && x /= a = 1 + cant_divisores xs a | otherwise = cant_divisores xs a
+
+hallarTupla::[[Int]]->[(Int,Int,Int,Int)]
+hallarTupla lista = [(menor x ,mayor x ,cant_multiplos x (menor x) , cant_divisores x (mayor x))| x <- lista]
